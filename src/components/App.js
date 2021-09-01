7195834b493faa187042914ee0f59960cddb5150
Aniket Waliyan <30599754+Aniket-Wali@users.noreply.github.com>
2021-09-01 14:33:52 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,25 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+onst handleRestart = ()=>{
+      setIndex(0);
+  }
 
-const App = () => {
   return (
-    <></>
+    <>
+    <div className='App'>
+      <div id="slide">
+      <h1 data-testid="title">
+        {slides[index].title}
+      </h1>
+       <p data-testid="text">
+       {slides[index].text}
+       </p>
+       <div id='navigation'>
+       {index===0?<button type="button" disabled data-testid="button-prev" onClick={handlePrev}>Previous</button>:<button type="button"  data-testid="button-prev" onClick={handlePrev}>Previous</button>} 
+       {index===0?<button type="button" disabled data-testid="button-restart" onClick={handleRestart}>Restart</button>:<button type="button"  data-testid="button-restart" onClick={handleRestart}>Restart</button>}
+        {index===slides.length-1?<button type="button" disabled data-testid="button-next" onClick={handleNext}>Next</button>:<button type="button" data-testid="button-next" onClick={handleNext}>Next</button>}
+       </div>
+       </div>
+    </div>
+    </>
   )
 }
 
